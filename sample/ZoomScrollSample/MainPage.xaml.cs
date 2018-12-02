270a136657ee2f0cd7cebfcf7cb1175c49c3e09e
Sylvain Gravel <sgravel@lerondpoint.net>
2018-12-02 16:16:49 -0500

    Empty library project and sample intent

---
+++ sample/ZoomScrollSample/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace ZoomScrollSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PinchGestureRecognizer_OnPinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
+        {
+            Scroller.Content.Scale += (e.Scale - 1);
+        }
+    }
+}
