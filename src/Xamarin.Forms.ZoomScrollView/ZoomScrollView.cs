e106a50fb976ca74b291c80f971a0c187ee818da
Sylvain Gravel <sgravel@lerondpoint.net>
2019-01-05 11:37:40 -0500

    Remove unused pinch gesture

--- src/Xamarin.Forms.ZoomScrollView/ZoomScrollView.cs
+++ src/Xamarin.Forms.ZoomScrollView/ZoomScrollView.cs
@@ -1,13 +1,9 @@
-﻿using System;
-
-namespace Xamarin.Forms.ZoomScrollView
+﻿namespace Xamarin.Forms.ZoomScrollView
 {
     public class ZoomScrollView : ScrollView
     {
         public static readonly BindableProperty MinimumZoomScaleProperty = BindableProperty.Create(nameof(MinimumZoomScale), typeof(float), typeof(ZoomScrollView), 1.0f);
         public static readonly BindableProperty MaximumZoomScaleProperty = BindableProperty.Create(nameof(MaximumZoomScale), typeof(float), typeof(ZoomScrollView), 1.0f);
-
-        PinchGestureRecognizer pinch = new PinchGestureRecognizer();
 
         public float MinimumZoomScale
         {
@@ -21,35 +17,8 @@ namespace Xamarin.Forms.ZoomScrollView
             set => SetValue(MaximumZoomScaleProperty, value);
         }
 
-        public ZoomScrollView() : base()
+        public ZoomScrollView()
         {
-            pinch.PinchUpdated += HandlePinchZoom;
-        }
-
-        protected override void OnPropertyChanging(string propertyName = null)
-        {
-            base.OnPropertyChanging(propertyName);
-
-            if (propertyName == nameof(Content))
-            {
-                Content?.GestureRecognizers.Remove(pinch);
-            }
-        }
-
-        protected override void OnPropertyChanged(string propertyName = null)
-        {
-            base.OnPropertyChanged(propertyName);
-
-            if (propertyName == nameof(Content))
-            {
-                //Content?.GestureRecognizers.Add(pinch);
-            }
-        }
-
-        private void HandlePinchZoom(object sender, PinchGestureUpdatedEventArgs e)
-        {
-            Console.WriteLine($"New zoom = {e.Scale} | ContentSize = {ContentSize}");
-            Content.Scale += (e.Scale - 1);
         }
     }
 }
